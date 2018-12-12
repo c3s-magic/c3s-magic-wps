6f90f6a0b08d940f1e064ec991e36fdabd60c05d
Niels Drost <n.drost@esciencecenter.nl>
2018-12-12 16:39:31 +0000

    added cvdp metric

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -1,6 +1,7 @@
 from .wps_sleep import Sleep
 from .wps_python_example import PythonExample
 from .wps_consecdrydays import ConsecDryDays
+from .wps_cvdp import CVDP
 from .wps_perfmetrics import Perfmetrics
 from .wps_rainfarm import RainFarm
 from .wps_rmse import RMSE
@@ -13,6 +14,7 @@ processes = [
     Sleep(),
     PythonExample(),
     ConsecDryDays(),
+    CVDP(),
     RainFarm(),
     RMSE(),
 ]
