f80b6572c0a813992a1c351a609acf939da97f4c
Niels Drost <n.drost@esciencecenter.nl>
2018-12-12 18:47:29 +0000

    added shapeselect metric

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -3,6 +3,7 @@ from .wps_python_example import PythonExample
 from .wps_consecdrydays import ConsecDryDays
 from .wps_cvdp import CVDP
 from .wps_ensclus import EnsClus
+from .wps_shapeselect import ShapeSelect
 from .wps_perfmetrics import Perfmetrics
 from .wps_rainfarm import RainFarm
 from .wps_rmse import RMSE
@@ -17,6 +18,7 @@ processes = [
     ConsecDryDays(),
     CVDP(),
     EnsClus(),
+    ShapeSelect(),
     RainFarm(),
     RMSE(),
 ]
