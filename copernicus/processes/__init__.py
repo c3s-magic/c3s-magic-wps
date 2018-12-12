35a00d712dc31d5f9e46adccfea7fbd3eea23839
Niels Drost <n.drost@esciencecenter.nl>
2018-12-12 18:17:42 +0000

    added ensclus metric

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -2,6 +2,7 @@ from .wps_sleep import Sleep
 from .wps_python_example import PythonExample
 from .wps_consecdrydays import ConsecDryDays
 from .wps_cvdp import CVDP
+from .wps_ensclus import EnsClus
 from .wps_perfmetrics import Perfmetrics
 from .wps_rainfarm import RainFarm
 from .wps_rmse import RMSE
@@ -15,6 +16,7 @@ processes = [
     PythonExample(),
     ConsecDryDays(),
     CVDP(),
+    EnsClus(),
     RainFarm(),
     RMSE(),
 ]
