bd46ce2442b8da1c313115615f8631efe053ca5b
Niels Drost <n.drost@esciencecenter.nl>
2019-05-14 15:35:24 +0100

    added wps cmip selection

--- sync-scripts/wps-data-finder/find-data.sh
+++ sync-scripts/wps-data-finder/find-data.sh
@@ -6,17 +6,16 @@ root=/group_workspaces/jasmin2/cp4cds1/data/c3s-cmip5/output1
 
 ens='r[12]i1p1'
 
-
 for freq in mon day; do
     file="${freq}_files.txt"
     rm -f "$file"
     for model in $(cat models.txt); do
 	for exp in historical rcp85; do
             for var in $(cat "${freq}_variables.txt"); do
-		path="$root/*/$model/$exp/$freq/*/*/$ens/$var/latest/*.nc"
+		path="${root}/*/$model/$exp/$freq/*/*/$ens/$var/latest/"
 		echo Reading "$path"
 		du -hscL $path | tail -n 1
-		ls -1 $path >> "$file"
+		ls -d $path >> "$file"
             done
 	done
     done
@@ -25,3 +24,6 @@ for freq in mon day; do
     tr '\n' '\0' < "$file" > "${file}0"
     du -hscL --files0-from="${file}0" | tail -n 1
 done
+
+cat mon_files.txt day_files.txt > selection.txt
+sed -i "s#${root}/##g" selection.txt
