9c0dc7db0c258e88b39c8166f259cd45874f1634
Berend Weel <b.weel@esciencecenter.nl>
2019-05-22 14:14:28 +0200

    moved mip and added model selection text

--- c3s_magic_wps/processes/wps_zmnam.py
+++ c3s_magic_wps/processes/wps_zmnam.py
@@ -14,7 +14,7 @@ LOGGER = logging.getLogger("PYWPS")
 class ZMNAM(Process):
     def __init__(self):
         inputs = [
-            *model_experiment_ensemble(model='	MPI-ESM-MR', experiment='historical', ensemble='r1i1p1'),
+            *model_experiment_ensemble(model='MPI-ESM-MR', experiment='historical', ensemble='r1i1p1'),
             *year_ranges((1979, 2005)),
         ]
         self.pressure_levels = [5000, 25000, 50000, 100000]
@@ -59,9 +59,16 @@ class ZMNAM(Process):
             abstract="Stratosphere-troposphere coupling and annular modes indices (ZMNAM)",
             metadata=[
                 Metadata('ESMValTool', 'http://www.esmvaltool.org/'),
-                Metadata('Documentation',
-                         'https://esmvaltool.readthedocs.io/en/version2_development/recipes/recipe_zmnam.html',
-                         role=util.WPS_ROLE_DOC)
+                Metadata(
+                    'Documentation',
+                    'https://esmvaltool.readthedocs.io/en/version2_development/recipes/recipe_zmnam.html',
+                    role=util.WPS_ROLE_DOC,
+                ),
+                Metadata(
+                    'Model Selection',
+                    """The ZMNAM metric accepts one model as the input. Any models beyond
+                    the first are ignored.""",
+                )
             ],
             inputs=inputs,
             outputs=outputs,
