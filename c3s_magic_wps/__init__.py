56c397ce420ff4fdff34d1fccdc7826ae9db546f
Berend Weel <b.weel@esciencecenter.nl>
2019-05-22 11:40:02 +0200

    Added canary cli script as a very basic wps interface

--- c3s_magic_wps/__init__.py
+++ c3s_magic_wps/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for c3s magic wps."""
 
 from .__version__ import __author__, __email__, __version__
 
-from .wsgi import application
+from .wsgi import create_app
