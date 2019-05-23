5e78378ab87da83c5aa5e5e1115e593fd05f1114
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-05-23 10:28:30 +0200

    update heatwaves_coldwaves

--- c3s_magic_wps/processes/wps_heatwaves_coldwaves.py
+++ c3s_magic_wps/processes/wps_heatwaves_coldwaves.py
@@ -15,15 +15,16 @@ LOGGER = logging.getLogger("PYWPS")
 class HeatwavesColdwaves(Process):
     def __init__(self):
         inputs = [
-            *model_experiment_ensemble(model='bcc-csm1-1', experiment='rcp85', ensemble='r1i1p1'),
-            *year_ranges((1971, 2000), start_name='start_historical', end_name='end_historical'),
-            *year_ranges((2020, 2050), start_name='start_projection', end_name='end_projection'),
-            # *model_experiment_ensemble(model_name='Model_historical',
-            #                            experiment_name='Experiment_historical',
-            #                            ensemble_name='Ensemble_historical'),
-            # *model_experiment_ensemble(model_name='Model_projection',
-            #                            experiment_name='Experiment_projection',
-            #                            ensemble_name='Ensemble_projection'),
+            *model_experiment_ensemble(model='bcc-csm1-1',
+                                       experiment='rcp85',
+                                       ensemble='r1i1p1',
+                                       max_occurs=1),
+            *year_ranges((1971, 2000),
+                         start_name='start_historical',
+                         end_name='end_historical'),
+            *year_ranges((2020, 2050),
+                         start_name='start_projection',
+                         end_name='end_projection'),
             LiteralInput('quantile',
                          'Quantile',
                          abstract='quantile defining the exceedance/non-exceedance threshold',
@@ -93,15 +94,14 @@ class HeatwavesColdwaves(Process):
         response.update_status("starting ...", 0)
 
         # build esgf search constraints
-        constraints = dict(model_historical=request.inputs['model_historical'][0].data,
-                           ensemble_historical=request.inputs['ensemble_historical'][0].data,
-                           start_year_historical=request.inputs['start_historical'][0].data,
-                           end_year_historical=request.inputs['end_historical'][0].data,
-                           model_projection=request.inputs['model_projection'][0].data,
-                           experiment_projection=request.inputs['experiment_projection'][0].data,
-                           ensemble_projection=request.inputs['ensemble_projection'][0].data,
-                           start_year_projection=request.inputs['start_projection'][0].data,
-                           end_year_projection=request.inputs['end_projection'][0].data)
+        constraints = dict(
+            model=request.inputs['model'][0].data,
+            ensemble=request.inputs['ensemble'][0].data,
+            experiment=request.inputs['experiment'][0].data,
+            start_year_historical=request.inputs['start_historical'][0].data,
+            end_year_historical=request.inputs['end_historical'][0].data,
+            start_year_projection=request.inputs['start_projection'][0].data,
+            end_year_projection=request.inputs['end_projection'][0].data)
 
         op = request.inputs['operator'][0].data
         if op == 'exceedances':
@@ -152,36 +152,42 @@ class HeatwavesColdwaves(Process):
         response.outputs['debug_log'].output_format = FORMATS.TEXT
         response.outputs['debug_log'].file = result['debug_logfile']
 
-        if not result['success']:
+        if result['success']:
+            try:
+                self.get_outputs(result, constraints, response)
+            except Exception as e:
+                response.update_status("exception occured: " + str(e), 85)
+                LOGGER.exception('Getting output failed: ' + str(e))
+        else:
             LOGGER.exception('esmvaltool failed!')
-            response.update_status("exception occured: " + result['exception'], 100)
-            return response
-
-        try:
-            self.get_outputs(result, response)
-        except Exception as e:
-            response.update_status("exception occured: " + str(e), 85)
+            response.update_status("exception occured: " + result['exception'],
+                                   85)
 
         response.update_status("creating archive of diagnostic result ...", 90)
 
         response.outputs['archive'].output_format = Format('application/zip')
-        response.outputs['archive'].file = runner.compress_output(os.path.join(self.workdir, 'output'),
-                                                                  'heatwaves_coldwaves_result.zip')
+        response.outputs['archive'].file = runner.compress_output(
+            os.path.join(self.workdir, 'output'),
+            'heatwaves_coldwaves_result.zip')
 
         response.update_status("done.", 100)
         return response
 
-    def get_outputs(self, result, response):
+    def get_outputs(self, result, constraints, response):
         # result plot
         response.update_status("collecting output ...", 80)
         response.outputs['plot'].output_format = Format('application/png')
-        response.outputs['plot'].file = runner.get_output(result['plot_dir'],
-                                                          path_filter=os.path.join('heatwaves_coldwaves', 'main'),
-                                                          name_filter="*extreme_spell*",
-                                                          output_format="png")
+        response.outputs['plot'].file = runner.get_output(
+            result['plot_dir'],
+            path_filter=os.path.join('heatwaves_coldwaves', 'main'),
+            name_filter="*extreme_spell*",
+            output_format="png"
+        )
 
         response.outputs['data'].output_format = FORMATS.NETCDF
-        response.outputs['data'].file = runner.get_output(result['work_dir'],
-                                                          path_filter=os.path.join('heatwaves_coldwaves', 'main'),
-                                                          name_filter="*extreme_spell*",
-                                                          output_format="nc")
+        response.outputs['data'].file = runner.get_output(
+            result['work_dir'],
+            path_filter=os.path.join('heatwaves_coldwaves', 'main'),
+            name_filter="*extreme_spell*",
+            output_format="nc"
+        )
