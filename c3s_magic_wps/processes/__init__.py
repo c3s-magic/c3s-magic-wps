6f3b32541354c20f24fcd535c1485d5cd1d0bebe
Berend Weel <b.weel@esciencecenter.nl>
2019-05-27 14:29:13 +0200

    Smpi (#41)
    * First implementation of smpi
    
    * Updated recipe to latest version
    
    * Changed name of result zip
    
    * removed max_occurs
    
    * Added output plot

--- c3s_magic_wps/processes/__init__.py
+++ c3s_magic_wps/processes/__init__.py
@@ -19,6 +19,7 @@ from .wps_drought_indicator import DroughtIndicator
 from .wps_quantilebias import QuantileBias
 from .wps_toymodel import Toymodel
 from .wps_rainfarm import RainFARM
+from .wps_smpi import SMPI
 
 processes = sorted(
     [
@@ -43,6 +44,7 @@ processes = sorted(
         QuantileBias(),
         RainFARM(),
         Toymodel(),
+        SMPI(),
     ],
     key=lambda process: process.title,
 )
