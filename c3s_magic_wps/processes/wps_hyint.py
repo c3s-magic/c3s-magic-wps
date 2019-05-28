d0d17387e18a68d924a81c0ddef3682e092a60d9
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2019-05-28 15:21:19 +0200

    fixed missing .util import

--- c3s_magic_wps/processes/wps_hyint.py
+++ c3s_magic_wps/processes/wps_hyint.py
@@ -5,7 +5,7 @@ from pywps import FORMATS, ComplexInput, ComplexOutput, Format, LiteralInput, Li
 from pywps.app.Common import Metadata
 from pywps.response.status import WPS_STATUS
 
-from .utils import default_outputs, model_experiment_ensemble, outputs_from_plot_names
+from .utils import default_outputs, model_experiment_ensemble, outputs_from_plot_names, year_ranges
 
 from .. import runner, util
 
