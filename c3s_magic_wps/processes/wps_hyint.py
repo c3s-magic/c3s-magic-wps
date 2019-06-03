536097e0d05c9e907cf752683e7ccd002ec78d12
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-06-03 17:07:08 +0200

    add missing regions

--- c3s_magic_wps/processes/wps_hyint.py
+++ c3s_magic_wps/processes/wps_hyint.py
@@ -44,7 +44,7 @@ class HyInt(Process):
                 'Regions',
                 abstract='regions for timeseries and maps',
                 data_type='string',
-                allowed_values=["GL", "SA", "AF", "EU", "EA"],
+                allowed_values=["GL", "GL60", "TR", "SA", "AF", "IN", "EU", "EA", "AU"],
                 default="GL",
                 min_occurs=1,
                 max_occurs=5,
